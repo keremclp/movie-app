41db6bf4d45c3cde0e12483ce6779c94f1861818
keremclp <kerem00962@gmail.com>
2025-03-26 01:30:04 +0300

    feat: setup styling process

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center bg-white dark:bg-black"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text 
+        className="text-5xl text-dark-200 font-bold"
+      >
+        Welcome!
+      </Text>
     </View>
   );
 }
